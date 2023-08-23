49105a99642547263fc1aa684f14d0a12335b7bb
prettyturtle <unifconti@gmail.com>
2023-08-24 01:25:04 +0900

    Initial Commit

---
+++ yc_Touchdown/yc_TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  yc_TouchdownApp.swift
+//  yc_Touchdown
+//
+//  Created by yc on 2023/08/24.
+//
+
+import SwiftUI
+
+@main
+struct yc_TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
