1d1a1d55c50ec14b4e22c60226ebf052e9538e13
prettyturtle <unifconti@gmail.com>
2023-08-24 01:43:40 +0900

    First Commit : Assets/FooterView

---
+++ yc_Touchdown/Utility/Constant.swift
@@ -0,0 +1,37 @@
+//
+//  Constant.swift
+//  yc_Touchdown
+//
+//  Created by yc on 2023/08/24.
+//
+
+import SwiftUI
+
+// MARK: - DATA
+
+
+// MARK: - COLOR
+let colorBackground: Color = Color("ColorBackground")
+let colorGray: Color = Color(UIColor.systemGray4)
+
+// MARK: - LAYOUT
+
+
+// MARK: - UX
+
+
+// MARK: - API
+
+
+// MARK: - IMAGE
+
+
+// MARK: - FONT
+
+
+// MARK: - STRING
+
+
+// MARK: - MISC
+
+
